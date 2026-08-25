a9be3af155513744d94fd849a12796b48eabf900
Jan Simko <205554583+neldoretpo@users.noreply.github.com>
2026-08-25 23:48:49 +0200

    edit text

--- src/pages/VzdelavanieCirkus.jsx
+++ src/pages/VzdelavanieCirkus.jsx
@@ -15,43 +15,43 @@ const VzdelavanieCirkus = () => {
         <br></br><br></br>
           Vieme poskytnúť programy od najmenších detí s rodičmi, cez dospelých až po seniorov, vrátane znevýhodnených skupín.<br></br>
 <br></br>
-<h3>aktuálna ponuka kurzov:</h3>
+<h3>Aktuálna ponuka kurzov:</h3>
 <br></br>
-Cirkusový krúžok v škôlke (3-6r)
-Pondelky 8:15-9:00
-Súkromná MŠ M. Montessori
-Cena: 105 €
+Cirkusový krúžok v škôlke (3-6r)<br></br>
+Pondelky 8:15-9:00<br></br>
+Súkromná MŠ M. Montessori<br></br>
+Cena: 105 €<br></br>
+<br></br>
 
+Cirkus pre rodičov s deťmi (3-6r)<br></br>
+Piatky 14:30-15:30<br></br>
+Playroom (OD Kocka)<br></br>
+Cena: 190 € (pre rodiča s jedným dieťaťom. pri viacerých deťoch je možná zľava)<br></br>
+<br></br>
 
-Cirkus pre rodičov s deťmi (3-6r)
-Piatky 14:30-15:30
-Playroom (OD Kocka)
-Cena: 190 € (pre rodiča s jedným dieťaťom. pri viacerých deťoch je možná zľava)
+Adaptovaný cirkus (7-20r)<br></br>
+Štvrtky 14:30-15:30<br></br>
+Telocvičňa Obchodnej akadémie<br></br>
+Cena: 190 €<br></br>
+Tieto kurzy sú vhodné pre klientov s akýmikoľvek špeciálnymi potrebami<br></br>
 
-
-Adaptovaný cirkus (7-20r)
-Štvrtky 14:30-15:30
-Telocvičňa Obchodnej akadémie
-Cena: 190 €
-Tieto kurzy sú vhodné pre klientov s akýmikoľvek špeciálnymi potrebami
-
-
-Úvod do cirkusu (7-20r)
-Štvrtky 15:30-17:00
-Telocvičňa Obchodnej akadémie
-Cena: 240 €
-
-Pokročilá cirkusová družina (Na pozvánku)
-Štvrtky 17:00-17:30
-Telocvičňa Obchodnej akadémie
-Cena: 0 €
-
-Cirkus pre dospelých (15-99r)
-Piatky 18:30-20:00
-Telocvičňa Obchodnej akadémie
-Cena: 240 €
-
-          Modely môžu byť nasledovné:<br></br>
+<br></br>
+Úvod do cirkusu (7-20r)<br></br>
+Štvrtky 15:30-17:00<br></br>
+Telocvičňa Obchodnej akadémie<br></br>
+Cena: 240 €<br></br>
+<br></br>
+Pokročilá cirkusová družina (Na pozvánku)<br></br>
+Štvrtky 17:00-17:30<br></br>
+Telocvičňa Obchodnej akadémie<br></br>
+Cena: 0 €<br></br>
+<br></br>
+Cirkus pre dospelých (15-99r)<br></br>
+Piatky 18:30-20:00<br></br>
+Telocvičňa Obchodnej akadémie<br></br>
+Cena: 240 €<br></br>
+<br></br><br></br>
+          Ďalšie odely môžu byť nasledovné:<br></br>
           (1) uzavretý workshop, kde je malý počet účasntíkov od začiatku do konca a vieme postupne zvyšovať náročnosť techník<br></br>
           (2) otvorený workshop na podujatí, kde účastníci rôzne prichádzajú a odchádzajú, je to skôr zážitkové ako budujúce zručnosti<br></br>
           (3) pravidelný kurz (aktuálne prebieha krúžok pre deti od 3-6 rokov v rámci MŠ; a krúžok pre deti a dospelých od 10 rokov vyššie)
